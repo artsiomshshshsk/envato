f3f22c901c9b6f0dce786585b7814a812cec1209
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2021-04-03 20:47:12 +0300

    some-stuff

--- envato.py
+++ envato.py
@@ -4,6 +4,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import platform
+from getpass import getuser
+from selenium.webdriver.chrome.options import Options
+import subprocess
+import names
 
 
 def wait_element(x_path):
@@ -15,6 +19,10 @@ def wait_element(x_path):
         return element
     except:
         return 0
+
+def get_name():
+    return names.get_full_name( gender='male')
+
 
 categories = {
     "house":"https://elements.envato.com/audio/genre-house/min-length-01:30/max-length-03:00/sort-by-latest",
@@ -34,16 +42,12 @@ if os_info == 'Windows':
 else:
     ex_path = '/Users/artsiom/Music-Botting/envato/chromedriver'
 
-
-driver = webdriver.Chrome(executable_path=ex_path)
-
-#/html/body/div[2]/div[1]/main/div/div/section/div/div[3]/div[3]/div[1]/ul/li[1]/div/a
-#/html/body/div[2]/div[1]/main/div/div/section/div/div[3]/div[3]/div[1]/ul/li[2]/div/a
-#/html/body/div[2]/div[1]/main/div/div/section/div/div[3]/div[3]/div[1]/ul/li[24]/div/a
-
-
-
-
+subprocess.call("TASKKILL /f  /IM  CHROME.EXE")
+subprocess.call("TASKKILL /f  /IM  CHROMEDRIVER.EXE")
+chrome_options = Options()
+chrome_options.add_argument(r"user-data-dir=C:\Users\%s\AppData\Local\Google\Chrome\User Data" % getuser())
+driver = webdriver.Chrome(executable_path=ex_path,chrome_options=chrome_options)
+DOWNLOADS = r"C:\Users\Artsiom\Desktop\git_envato\downloads"
 
 
 for each in categories:
@@ -61,9 +65,23 @@ for each in categories:
             element = wait_element(f'/html/body/div[2]/div[1]/main/div/div/section/div/div[3]/div[3]/div[1]/ul/li[{i}]/div/a')
             element.location_once_scrolled_into_view
             href = element.get_attribute('href')
-            sleep(0.5)
+            driver.execute_script( "window.open('');" )
+            driver.switch_to.window(driver.window_handles[1] )
+            driver.get(href)
+
             print(href)
 
+
+            project_name = get_name()
+            download_button = wait_element('/html/body/div[2]/div[1]/main/div/div/div[1]/div/div/div[2]/div[2]/div[2]/button')
+            download_button.click()
+            create_new_project = wait_element('/html/body/div[8]/div/div/div/div/div/form/div[1]/div[2]/div/div[3]/div[2]')
+            create_new_project.click()
+            project_name_input = wait_element('/html/body/div[8]/div/div/div/div/div/form/div[1]/div[2]/div/div[1]/div/div[2]/input')
+
+            project_name_input.send_keys(project_name)
+            driver.close()
+            driver.switch_to.window( driver.window_handles[0] )
         try:
             next_but = driver.find_element_by_link_text(f'{link_number}')
                                                     
