6a05399595ba40d432d8d19ca6288da1b71fbc4a
unknown <you@example.com>
2021-04-09 13:55:11 +0200

    'captcha-to-do'

--- envato.py
+++ envato.py
@@ -1,3 +1,4 @@
+import traceback
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
@@ -16,18 +17,29 @@ import wave
 import contextlib
 import shutil
 import random
+import sqlite3
 
 
+
+def detect_and_solve_captcha():
+    global driver
+    try:
+        status = driver.find_element_by_class_name('status')
+    except:
+
+        breakpoint()
 
 
 def add_url_to_db(url):
-    pass
+    curs.execute('insert into songs values (?)', [url])
+    conn.commit()
 
 
 def if_url_already_in_db(url):
-    # true        уже в базе
-    # false       нету в базе
-    pass
+   curs.execute('select * from songs')
+   urls = [x[0] for x in curs.fetchall()]
+   if url in urls:
+       return True
 
 
 def get_wav_duration(path):
@@ -55,9 +67,11 @@ def get_name():
     return names.get_full_name( gender='male')
 
 def remove_all_files_in_directory(path):
-    for file in os.listdir(path):
-        file = os.path.join(path,file)
-        os.remove(file)
+    for root, dirs, files in os.walk(r"C:\Users\PC1\Documents\EnvatoDownloader\envato\downloads", topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+           shutil.rmtree(os.path.join(root, name))
 
 
 
@@ -72,6 +86,13 @@ categories = {
 }
 os_info = platform.system()
 print(os_info)
+DB_PATH = 'songs.db'
+created = DB_PATH in os.listdir()
+conn = sqlite3.connect(DB_PATH)
+curs = conn.cursor()
+if not created:
+    curs.execute('create table songs (url varchar(255));')
+
 
 
 downloads_dir = os.path.join(get_script_path(),'downloads')
@@ -85,8 +106,6 @@ songs = os.path.join(get_script_path(), 'songs')
 if not os.path.isdir(songs):
     os.mkdir(songs)
     print('Songs folder is created.')
-
-
 
 
 if os_info == 'Windows':
@@ -103,8 +122,6 @@ else:
     user_data_dir = r"user-data-dir=/Users/%s/Library/Application Support/Google/Chrome/Default" % getuser()
     print(user_data_dir)
 
-#if database is not created 
-#create here
 
 
 chrome_options = Options()
@@ -141,6 +158,7 @@ for each in all_genres:
             #href = url of song to check 
 
             if if_url_already_in_db(href):
+                print(f'{href} IS ALREADY DOWNLOADED')
                 continue
 
             driver.execute_script( "window.open('');" )
@@ -149,23 +167,27 @@ for each in all_genres:
 
             print(href)
 
-
             project_name = get_name()
             download_button = wait_element('/html/body/div[2]/div[1]/main/div/div/div[1]/div/div/div[2]/div[2]/div[2]/button')
+            download_button.location_once_scrolled_into_view
+            while True:
+                try:
+                    pyautogui.press('pageup')
+                    sleep(1)
+                    download_button.click()
+                    sleep(2)
+                    create_new_project = wait_element('/html/body/div[8]/div/div/div/div/div/form/div[1]/div[2]/div/div[3]/div[2]')
+                    create_new_project.click()
+                    project_name_input = wait_element('/html/body/div[8]/div/div/div/div/div/form/div[1]/div[2]/div/div[1]/div/div[2]/input')
+                    sleep(2)
 
-            pyautogui.press( 'pageup' )
-            sleep(1)
-            download_button.click()
-            sleep(2)
-            create_new_project = wait_element('/html/body/div[8]/div/div/div/div/div/form/div[1]/div[2]/div/div[3]/div[2]')
-            create_new_project.click()
-            project_name_input = wait_element('/html/body/div[8]/div/div/div/div/div/form/div[1]/div[2]/div/div[1]/div/div[2]/input')
-            sleep( 2 )
-
-            project_name_input.send_keys(project_name)
-            submit = wait_element('/html/body/div[8]/div/div/div/div/div/form/div[2]/button')
-            remove_all_files_in_directory(downloads_dir)
-            submit.click()
+                    project_name_input.send_keys(project_name)
+                    submit = wait_element('/html/body/div[8]/div/div/div/div/div/form/div[2]/button')
+                    remove_all_files_in_directory(downloads_dir)
+                    submit.click()
+                    break
+                except Exception:
+                    traceback.print_exc()
             # waiting for file to be downloaded
            
             file_is_downloaded = False
@@ -191,9 +213,7 @@ for each in all_genres:
                         break
             remove_all_files_in_directory(downloads_dir)
 
-            #adding url to db
             add_url_to_db(href)
-
 
             driver.close()
             driver.switch_to.window( driver.window_handles[0] )
